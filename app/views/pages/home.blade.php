abb8ad5afe19aeebcd95c8f1b5e13a18cef47016
Vadim Polh <vadimpolh@gmail.com>
2014-08-14 03:41:55 +0300

    Initial commit

---
+++ app/views/pages/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.default')
+
+@section('content')
+    <div class="jumbotron">
+         <h1>Welcome to Larabook!</h1>
+         <p>Welcome to the premier place to talk about laravel with others!</p>
+          <p><a class="btn btn-primary btn-lg" role="button">Sign Up!</a></p>
+    </div>
+
+
+@stop
+
---
+++ info.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 13/08/14
+ * Time: 22:58
+ */
+
+phpinfo();
