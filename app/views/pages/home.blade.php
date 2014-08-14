eb13beda9802f0dac4c8eac4457e55e03e956e9d
Vadim Polh <vadimpolh@gmail.com>
2014-08-14 05:06:07 +0300

    Add gulp functional

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -4,7 +4,9 @@
     <div class="jumbotron">
          <h1>Welcome to Larabook!</h1>
          <p>Welcome to the premier place to talk about laravel with others!</p>
-          <p><a class="btn btn-primary btn-lg" role="button">Sign Up!</a></p>
+         <p>
+         {{link_to_route('register_path','Sign Up!', null, ['class'=>'btn btn-primary btn-lg'])}}
+         </p>
     </div>
 
 
